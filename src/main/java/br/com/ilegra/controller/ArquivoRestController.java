0a477073350e105e1c1d0f5c7e7fce7c000b9659
Leonny Lino <leonny.1993@gmail.com>
2021-10-20 17:57:48 -0300

    *Add instruções *Ajuste no codigo

--- src/main/java/br/com/ilegra/controller/ArquivoRestController.java
+++ src/main/java/br/com/ilegra/controller/ArquivoRestController.java
@@ -1,6 +1,5 @@
 package br.com.ilegra.controller;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ public class ArquivoRestController {
 	private final ArquivoService aService;
 	
 
-	@Bean	
 	@PostMapping
 	public void uploadArquivo() {
 		aService.fileRead();
